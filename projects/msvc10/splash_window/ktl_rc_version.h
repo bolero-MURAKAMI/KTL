d4d36a12aaf2832a655d1f29f22f433de046f3b2
bolro-MURAKAMI <bolero.murakami@gmail.com>
2012-03-04 12:46:27 +0900

    msvc8, msvc10 追加

---
+++ projects/msvc10/splash_window/ktl_rc_version.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define KTL_RC_PROJECT_NAME		"splash_window"
+#define KTL_RC_MODULE_NAME		"SplashWindow"
+#define KTL_RC_VERSION			0,2,3,1
+#define KTL_RC_TARGET			"KiriKiri 2.32rev2 core"
+#define KTL_RC_COMPILER			"Microsoft Visual C++ version 10.0"
+#define KTL_RC_USELIB_0			"Windows SDK 7.1"
+#define KTL_RC_USELIB_1			"Boost 1.49"
+#define KTL_RC_USELIB_2			"zlib 1.2.6"
+#define KTL_RC_USELIB_3			"libpng 1.5.9"
