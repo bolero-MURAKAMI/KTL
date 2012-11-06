d9394dc4a3caf229429cc19af417cfdd610385b9
Bolero-MURAKAMI <bolero.murakami@gmail.com>
2012-11-06 22:55:59 +0900

    Ver 0.2.6 : Boost を Boost 1.51 から Boost 1.52 へアップデート。

--- projects/msvc11/octet_builder/ktl_rc_version.h
+++ projects/msvc11/octet_builder/ktl_rc_version.h
@@ -2,8 +2,8 @@
 
 #define KTL_RC_PROJECT_NAME		"octet_builder"
 #define KTL_RC_MODULE_NAME		"OctetBuilder"
-#define KTL_RC_VERSION			0,2,5,1
+#define KTL_RC_VERSION			0,2,6,0
 #define KTL_RC_TARGET			"KiriKiri 2.32rev2 core"
 #define KTL_RC_COMPILER			"Microsoft Visual C++ version 11.0"
 #define KTL_RC_USELIB_0			"Windows SDK 7.1"
-#define KTL_RC_USELIB_1			"Boost 1.51"
+#define KTL_RC_USELIB_1			"Boost 1.52"
--- projects/msvc9/serialization/ktl_rc_version.h
+++ projects/msvc9/serialization/ktl_rc_version.h
@@ -2,8 +2,8 @@
 
 #define KTL_RC_PROJECT_NAME		"serialization"
 #define KTL_RC_MODULE_NAME		"Serialization"
-#define KTL_RC_VERSION			0,2,5,1
+#define KTL_RC_VERSION			0,2,6,0
 #define KTL_RC_TARGET			"KiriKiri 2.32rev2 core"
 #define KTL_RC_COMPILER			"Microsoft Visual C++ version 9.0"
 #define KTL_RC_USELIB_0			"Windows SDK 7.1"
-#define KTL_RC_USELIB_1			"Boost 1.51"
+#define KTL_RC_USELIB_1			"Boost 1.52"
