c11401249ffee3e37de8974ab71e939ad0c00c86
Bolero-MURAKAMI <bolero.murakami@gmail.com>
2013-02-06 02:15:03 +0900

    update lib version

--- projects/msvc11/splash_window/ktl_rc_version.h
+++ projects/msvc11/splash_window/ktl_rc_version.h
@@ -2,10 +2,10 @@
 
 #define KTL_RC_PROJECT_NAME		"splash_window"
 #define KTL_RC_MODULE_NAME		"SplashWindow"
-#define KTL_RC_VERSION			0,2,6,0
+#define KTL_RC_VERSION			0,2,7,0
 #define KTL_RC_TARGET			"KiriKiri 2.32rev2 core"
 #define KTL_RC_COMPILER			"Microsoft Visual C++ version 11.0"
 #define KTL_RC_USELIB_0			"Windows SDK 7.1"
-#define KTL_RC_USELIB_1			"Boost 1.52"
+#define KTL_RC_USELIB_1			"Boost 1.53"
 #define KTL_RC_USELIB_2			"zlib 1.2.7"
-#define KTL_RC_USELIB_3			"libpng 1.5.13"
+#define KTL_RC_USELIB_3			"libpng 1.5.14"
--- projects/msvc11/storage_stream/ktl_rc_version.h
+++ projects/msvc11/storage_stream/ktl_rc_version.h
@@ -2,8 +2,8 @@
 
 #define KTL_RC_PROJECT_NAME		"storage_stream"
 #define KTL_RC_MODULE_NAME		"StorageStream"
-#define KTL_RC_VERSION			0,2,6,0
+#define KTL_RC_VERSION			0,2,7,0
 #define KTL_RC_TARGET			"KiriKiri 2.32rev2 core"
 #define KTL_RC_COMPILER			"Microsoft Visual C++ version 11.0"
 #define KTL_RC_USELIB_0			"Windows SDK 7.1"
-#define KTL_RC_USELIB_1			"Boost 1.52"
+#define KTL_RC_USELIB_1			"Boost 1.53"
