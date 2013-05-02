74e9da6cded09dc085648e4afccbc54697e2e323
Bolero-MURAKAMI <bolero.murakami@gmail.com>
2013-05-03 01:29:51 +0900

    update 3rd-perty libraries

--- projects/msvc8/big_int/ktl_rc_version.h
+++ projects/msvc8/big_int/ktl_rc_version.h
@@ -2,9 +2,9 @@
 
 #define KTL_RC_PROJECT_NAME		"big_int"
 #define KTL_RC_MODULE_NAME		"BigInt"
-#define KTL_RC_VERSION			0,2,7,1
+#define KTL_RC_VERSION			0,2,7,2
 #define KTL_RC_TARGET			"KiriKiri 2.32rev2 core"
 #define KTL_RC_COMPILER			"Microsoft Visual C++ version 8.0"
 #define KTL_RC_USELIB_0			"Windows SDK 7.1"
 #define KTL_RC_USELIB_1			"Boost 1.53"
-#define KTL_RC_USELIB_2			"NTL 5.5.2"
+#define KTL_RC_USELIB_2			"NTL 6.0.0"
--- projects/msvc9/compression/ktl_rc_version.h
+++ projects/msvc9/compression/ktl_rc_version.h
@@ -2,9 +2,9 @@
 
 #define KTL_RC_PROJECT_NAME		"compression"
 #define KTL_RC_MODULE_NAME		"Compression"
-#define KTL_RC_VERSION			0,2,7,0
+#define KTL_RC_VERSION			0,2,7,1
 #define KTL_RC_TARGET			"KiriKiri 2.32rev2 core"
 #define KTL_RC_COMPILER			"Microsoft Visual C++ version 9.0"
 #define KTL_RC_USELIB_0			"Windows SDK 7.1"
 #define KTL_RC_USELIB_1			"Boost 1.53"
-#define KTL_RC_USELIB_2			"zlib 1.2.7"
+#define KTL_RC_USELIB_2			"zlib 1.2.8"
